c475b70971f80b23122d77ee70fdf68725211eae
Akstrov <yalam385.ya@gmail.com>
2023-12-21 13:52:06 +0000

    add rose-pine and some lsps

--- init.lua
+++ init.lua
@@ -189,15 +189,7 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
-
+  { 'rose-pine/neovim',     name = 'rose-pine' },
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -336,6 +328,65 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = highlight_group,
   pattern = '*',
 })
+
+--rose-pine
+require('rose-pine').setup({
+  --- @usage 'auto'|'main'|'moon'|'dawn'
+  variant = 'auto',
+  --- @usage 'main'|'moon'|'dawn'
+  dark_variant = 'main',
+  bold_vert_split = false,
+  dim_nc_background = false,
+  disable_background = false,
+  disable_float_background = false,
+  disable_italics = false,
+
+  --- @usage string hex value or named color from rosepinetheme.com/palette
+  groups = {
+    background = 'base',
+    background_nc = '_experimental_nc',
+    panel = 'surface',
+    panel_nc = 'base',
+    border = 'highlight_med',
+    comment = 'muted',
+    link = 'iris',
+    punctuation = 'subtle',
+
+    error = 'love',
+    hint = 'iris',
+    info = 'foam',
+    warn = 'gold',
+
+    headings = {
+      h1 = 'iris',
+      h2 = 'foam',
+      h3 = 'rose',
+      h4 = 'gold',
+      h5 = 'pine',
+      h6 = 'foam',
+    }
+    -- or set all headings at once
+    -- headings = 'subtle'
+  },
+
+  -- Change specific vim highlight groups
+  -- https://github.com/rose-pine/neovim/wiki/Recipes
+  highlight_groups = {
+    ColorColumn = { bg = 'rose' },
+
+    -- Blend colours against the "base" background
+    CursorLine = { bg = 'foam', blend = 10 },
+    StatusLine = { fg = 'love', bg = 'love', blend = 10 },
+
+    -- By default each group adds to the existing config.
+    -- If you only want to set what is written in this config exactly,
+    -- you can set the inherit option:
+    Search = { bg = 'gold', inherit = false },
+  }
+})
+
+-- Set colorscheme after options
+vim.cmd('colorscheme rose-pine')
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
@@ -565,10 +616,16 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
-  -- tsserver = {},
-  -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  tsserver = {},
+  html = { filetypes = { 'html', 'twig', 'hbs' } },
+  jsonls = {},
+  marksman = {},
+  intelephense = {},
+  sqlls = {},
+  tailwindcss = {},
+
 
   lua_ls = {
     Lua = {
